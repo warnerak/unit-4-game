10ebfc0b5210aaa8084498da589126d91a94dfee
unknown <warnerak@dukes.jmu.edu>
2019-03-06 20:03:07 -0500

    finished but not final push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 // Generate the random number
 
 $(document).ready(function() {
-    var randomNum = Math.floor(Math.random()*100+1)
+    var randomNum = Math.floor(Math.random()*100+10)
     console.log(randomNum);
 
 //Displaying the random number
@@ -10,10 +10,28 @@ $('#matchScore').text(randomNum);
 
 //Generate the random numbers for each of the crystals
 
-var rubyCrystal = Math.floor(Math.random());
-var amythestCrystal = Math.floor(Math.random());
-var emeraldCrystal = Math.floor(Math.random());
-var diamondCrystal = Math.floor(Math.random());
+Array.prototype.shuffle = function() {
+    var input = this;
+
+    for (var i = input.length-1; i >=0; i--){
+        var randomIndex = Math.floor(Math.random()*(i+1));
+        var itemAtIndex = input[randomIndex];
+
+        input[randomIndex] = input[i];
+        input[i] = itemAtIndex;
+    }
+   }
+
+var tempArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+tempArray.shuffle();
+
+console.log(tempArray);
+
+
+var rubyCrystal = tempArray[0];
+var amythestCrystal = tempArray[1];
+var emeraldCrystal = tempArray[2];
+var diamondCrystal = tempArray[3];
 
 // Wins, losses, and total variables
 
@@ -47,13 +65,29 @@ function reset () {
     randomNum = Math.floor(Math.random()*100+1);
     console.log(randomNum)
     $('#matchScore').text(randomNum);
-    rubyCrystal = Math.floor(Math.random());
+    Array.prototype.shuffle = function() {
+        var input = this;
+    
+        for (var i = input.length-1; i >=0; i--){
+            var randomIndex = Math.floor(Math.random()*(i+1));
+            var itemAtIndex = input[randomIndex];
+    
+            input[randomIndex] = input[i];
+            input[i] = itemAtIndex;
+        }
+       }
+    
+    var tempArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    tempArray.shuffle();
+    
+    console.log(tempArray);
+    rubyCrystal = tempArray[0];
     console.log(rubyCrystal);
-    amythestCrystal = Math.floor(Math.random());
+    amythestCrystal = tempArray[1];
     console.log(amythestCrystal);
-    emeraldCrystal = Math.floor(Math.random());
+    emeraldCrystal = tempArray[2];
     console.log(emeraldCrystal);
-    diamondCrystal = Math.floor(Math.random());
+    diamondCrystal = tempArray[3];
     console.log(diamondCrystal);
     totalCalc = 0;
     $('#totalScore').text(totalCalc);
